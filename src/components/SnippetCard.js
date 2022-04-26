ca87e79b68a127cd72ac050080aef270490f9052
Fran <francisco@altimari.nl>
2022-04-26 13:38:34 +0200

    Add tooltips and route to details page

--- src/components/SnippetCard.js
+++ src/components/SnippetCard.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import moment from 'moment';
-import {
-	Card, CardHeader, CardMedia, CardContent, CardActions,
-	Collapse, Avatar, IconButton, Typography, Divider, TextField, Box, CardActionArea
-} from '@mui/material';
 
 import { styled } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import {
+	Card, CardHeader, CardMedia, CardContent, CardActions,
+	Collapse, Avatar, IconButton, Typography, Box, Tooltip
+} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import StarIcon from '@mui/icons-material/Star';
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import LaunchIcon from '@mui/icons-material/Launch';
 
 import { useSelector } from 'react-redux';
 import { selectUser } from '../redux/selectors';
@@ -20,16 +22,20 @@ import { ReadEditor } from './'
 
 const ExpandMore = styled((props) => {
 	const { expand, ...other } = props;
-	return <IconButton {...other} />;
+	return (
+		<Tooltip title={expand ? "Hide" : "Show more"} placement="bottom">
+			<IconButton {...other} />
+		</Tooltip>
+	);
 })(({ theme, expand }) => ({
 	transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-	// marginLeft: 'auto',
 	transition: theme.transitions.create('transform', {
 		duration: theme.transitions.duration.shortest,
 	}),
 }));
 
 export const SnippetCard = ({ id, title, body, syntax, updatedAt, userId }) => {
+	const navigate = useNavigate();
 	const user = useSelector(selectUser);
 	const [expanded, setExpanded] = useState(false);
 
@@ -61,41 +67,43 @@ export const SnippetCard = ({ id, title, body, syntax, updatedAt, userId }) => {
 					</Typography>
 				</CardContent>
 			</Collapse>
-
-
 			{/* <CardMedia
 				component="img"
 				height="194"
 				image="/static/images/cards/paella.jpg"
 				alt="Paella dish"
 			/> */}
-			{/* <Box sx={{ mx: 2 }}>
-				<TextField
-					value={body.substring(0, 500)}
-					minRows={7}
-					maxRows={7}
-					fullWidth
-					multiline
-					// disabled
-					inputProps={{ readOnly: true, style: { fontSize: 12 } }}
-				/>
-			</Box> */}
 			<Box sx={{ mx: 2 }}>
 				<ReadEditor {...{ body, syntax }} />
 			</Box>
 
-			<CardActions>
-				<IconButton aria-label="add to favorites">
-					{user.id < id
-						? <StarIcon sx={{ color: Color.star }} />
-						: <StarOutlineIcon />
-					}
-				</IconButton>
-				23k
-				<IconButton aria-label="share">
-					<ContentCopyIcon />
-				</IconButton>
+			<CardActions disableSpacing sx={{ display: 'flex' }}>
+				{/* <Tooltip title="Star" placement="bottom">
+					<IconButton aria-label="add to favorites">
+						{user.id < id
+							? <StarIcon sx={{ color: Color.star }} />
+							: <StarOutlineIcon />
+						}
+					</IconButton>
+				</Tooltip>
+				23k */}
+
+				<Tooltip title="Copy" placement="bottom">
+					<IconButton aria-label="copy">
+						<ContentCopyIcon />
+					</IconButton>
+				</Tooltip>
+				<Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'flex-end' }}>
+					<Tooltip title="Details" placement="bottom">
+						<IconButton
+							onClick={() => navigate(`/snippets/:${id}/details`)}
+							aria-label="Go to details"
+						>
+							<LaunchIcon />
+						</IconButton>
+					</Tooltip>
+				</Box>
 			</CardActions>
-		</Card>
+		</Card >
 	);
 };
