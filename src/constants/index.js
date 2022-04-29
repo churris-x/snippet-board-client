b11ada9118d8bf738afdd893e3280c3d689d32c7
Fran <francisco@altimari.nl>
2022-04-29 13:39:54 +0200

    Change env variable name

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export * from './Color';
 export * from './Layout';
 
-export const API_URL = process.env.API_URL || "http://localhost:4055";
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4055";
 
 export const MESSAGE_TIMEOUT = 3000;
