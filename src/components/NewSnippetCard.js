a044c32cf5f1f6c7d4d6920b8c93004704b62391
Fran <francisco@altimari.nl>
2022-05-17 10:05:53 +0200

    Add snippet becomes FA button on small screens

--- src/components/NewSnippetCard.js
+++ src/components/NewSnippetCard.js
@@ -1,21 +1,37 @@
-import { Card, CardActionArea, CardHeader } from "@mui/material"
+import { useNavigate } from "react-router-dom";
+import { Box, Card, CardActionArea, CardHeader, Fab, Grid } from "@mui/material"
 import AddIcon from '@mui/icons-material/Add';
 
-export const NewSnippetCard = ({ onClick }) => {
+export const NewSnippetCard = () => {
+	const navigate = useNavigate();
+	const handleNew = () => navigate('/snippets/user/add');
+
 	return (
-		<Card sx={{ width: 345, height: 344 }}>
-			<CardActionArea onClick={onClick} sx={{
-				height: 'inherit',
-				display: 'flex',
-				justifyContent: 'flex-start',
-				flexDirection: 'column'
+		<>
+			<Fab color="primary" aria-label="add" onClick={handleNew} sx={{
+				position: 'fixed',
+				bottom: 32,
+				left: 'calc(50% - 28px)',
+				display: { xs: 'flex', sm: 'none' }
 			}}>
-				<CardHeader
-					title='New snippet'
-					sx={{ alignSelf: 'flex-start' }}
-				/>
-				<AddIcon sx={{ width: 40, height: 40, alignSelf: 'center', flexGrow: 1, mb: 8 }} />
-			</CardActionArea>
-		</Card>
+				<AddIcon />
+			</Fab>
+			<Grid item sx={{ display: { xs: 'none', sm: 'flex' } }}>
+				<Card sx={{ width: 345, height: 344 }}>
+					<CardActionArea onClick={handleNew} sx={{
+						height: 'inherit',
+						display: 'flex',
+						justifyContent: 'flex-start',
+						flexDirection: 'column'
+					}}>
+						<CardHeader
+							title='New snippet'
+							sx={{ alignSelf: 'flex-start' }}
+						/>
+						<AddIcon sx={{ width: 40, height: 40, alignSelf: 'center', flexGrow: 1, mb: 8 }} />
+					</CardActionArea>
+				</Card>
+			</Grid>
+		</>
 	)
 }
--- src/pages/UserSnippetsPage.js
+++ src/pages/UserSnippetsPage.js
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Container, CircularProgress, Grid, CardActionArea, Card, CardHeader, Button } from '@mui/material';
+import { Container, CircularProgress, Grid } from '@mui/material';
 
 import { selectIsLoading, selectPostSeach, selectToken, selectUserPosts } from '../redux/selectors';
 import { fetchUserPosts } from '../redux/actions';
@@ -10,15 +9,12 @@ import { fetchUserPosts } from '../redux/actions';
 import { NewSnippetCard, SnippetCard } from '../components';
 
 export const UserSnippetsPage = () => {
-	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
 	const token = useSelector(selectToken);
 	const isLoading = useSelector(selectIsLoading);
 	const posts = useSelector(selectUserPosts);
 	const search = useSelector(selectPostSeach);
-
-	const handleNew = () => navigate('/snippets/user/add');
 
 	useEffect(() => {
 		dispatch(fetchUserPosts());
@@ -28,11 +24,7 @@ export const UserSnippetsPage = () => {
 		<Container sx={{ flexDirection: isLoading ? 'column' : 'row', display: isLoading ? 'flex' : null }}>
 			{isLoading ? <CircularProgress sx={{ alignSelf: 'center', mt: 4 }} /> :
 				<Grid container spacing={4} my={4}>
-					{token &&
-						<Grid item>
-							<NewSnippetCard onClick={handleNew} />
-						</Grid>
-					}
+					{token && <NewSnippetCard />}
 					{!posts.length && token
 						? <Grid item>Couldn't find any posts!</Grid>
 						: posts.filter(i => i.title.toLowerCase().includes(search.toLowerCase())).map(post => (
