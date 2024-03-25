8d2e99ba1dd8224281ade3dc421843b1f3f4ee48
Francisco Altimari <francisco@altimari.nl>
2024-03-25 12:04:19 +0100

    Fix formating

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,11 +5,7 @@ import { userReducer as user } from './user/reducer';
 import { postReducer as post } from './post/reducer';
 import { appReducer as app } from './app/reducer';
 
-const reducer = combineReducers({
-    user,
-    post,
-    app,
-});
+const reducer = combineReducers({ user, post, app });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(applyMiddleware(thunk));
